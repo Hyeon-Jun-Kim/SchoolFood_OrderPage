28ed4e153491a7e7fe5f0d5dc19e7db054a8a078
Hyeon-Jun-Kim <gomji81@naver.com>
2022-08-11 10:40:11 +0900

    Initial Commit

---
+++ SchoolFood_OrderPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SchoolFood_OrderPage
+//
+//  Created by 김현준 on 2022/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SchoolFood_OrderPageUITests/SchoolFood_OrderPageUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SchoolFood_OrderPageUITestsLaunchTests.swift
+//  SchoolFood_OrderPageUITests
+//
+//  Created by 김현준 on 2022/08/11.
+//
+
+import XCTest
+
+class SchoolFood_OrderPageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
